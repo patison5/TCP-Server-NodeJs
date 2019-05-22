e0807272f64720c7c2f52d9c2ede6c6d186705ec
patison5 <patison4@yandex.ru>
2019-05-22 11:11:37 +0300

    finishing coursework

--- client.js
+++ client.js
@@ -14,7 +14,6 @@ colors = [
   '\x1b[36m%s\x1b[0m'
 ]
 
-// console.log('\x1b[36m%s\x1b[0m', 'I am cyan');
 
 var nickname = 'pid'
 var updateData = false;
@@ -24,86 +23,127 @@ function clearConsole () {
 	process.stdout.write('\033c');
 }
 
+rl.question(chalk.green(`Введите IP `), (IP) => {
+	rl.question(chalk.green(`Введите PORT `), (PORT) => {
 
-var client = net.connect({
-	port: 8282, 
-	// host: '46.39.41.8'
-  host: 'localhost'
-
-}, 
-function() {
-	console.log('\x1b[36m%s\x1b[0m', 'connected to server!'); 
-	checkRegister();
+		startMainLoop(IP, PORT);
+	});
 });
 
 
-function checkRegister() {
-	rl.question(chalk.red(`Введите никнейм `), (answer) => {
-		nickname = answer;
 
-		client.write(JSON.stringify({
-			'state': 'login',
-			'name': nickname
-		}))	
 
-		readAndSend();
-	});
-}
-
-client.on('data', function(data) {
-	var dataJSON = JSON.parse(data);
-
-	clearConsole();
-
-	for (let i = 0; i < dataJSON.length; i++) {
-		console.log('\x1b[33m%s\x1b[0m', `${dataJSON[i].name} > ${dataJSON[i].message}`);
-	}
+function startMainLoop (IP, PORT) {
 	
-	updateData = true;
+	console.log('Connecting to ', `${IP || 'localhost'}:${PORT || 8282}`)
 
-	if (updateData === true)
-		readAndSend();
-
-	// client.end();
-})
-
-
-
-
-
-function readAndSend () {
-	updateData = false;
-
-	rl.question(chalk.red(`${nickname} > `), (answer) => {
-
-		// var nickname = answer.split(' ')[0];
-
-		if (answer[0] == '#') {
-			answer = answer.split(' ');
-
-			var reciever = answer[0];
-				reciever = reciever.slice(1)
-
-			answer.splice(0, 1);
-			answer = answer.join(' ')
-
-			client.write(JSON.stringify({
-				'state': 'private',
-				'name': nickname,
-				'message': answer,
-				'reciever': reciever
-			}))	
-
-		} else {
-			client.write(JSON.stringify({
-				'state': 'broadcast',
-				'name': nickname,
-				'message': answer
-			}))	
-		}
-
-		
-
-		// readAndSend();
+	var client = net.connect({
+		port: PORT || 8282, 
+		// host: '46.39.41.8'
+		host: IP || 'localhost'
+	}, 
+	function() {
+		console.log(chalk.green('connected to server!')); 
+		checkRegister();
 	});
+
+
+	function checkRegister() {
+
+		console.log(
+			'\x1b[36m%s\x1b[0m',
+			'\n' +
+			'Welcome to my client-server courserwork!\n'+
+	        '\n' +
+	        '  ("`-’-/").___..--’’"`-._\n' +
+	        '   `6_ 6  )   `-.  (     ).`-.__.‘)\n' +
+	        '   (_Y_.)’  ._   )  `._ `. ``-..-’\n' +
+	        ' _..`--’_..-_/  /--’_.’ ,’\n' +
+	        '(il),-’‘  (li),’  ((!.-‘\n' +
+	        '\nCreated by ' +
+	        '— @patison5\n'
+	    );
+
+		rl.question(chalk.green(`Введите никнейм `), (answer) => {
+			nickname = answer.toUpperCase();
+
+			client.write(JSON.stringify({
+				'state': 'login',
+				'name': nickname
+			}))	
+
+			readAndSend();
+		});
+	}
+
+	client.on('data', function(data) {
+		var dataJSON = JSON.parse(data);
+
+		clearConsole();
+
+		for (let i = 0; i < dataJSON.length; i++) {
+			if (dataJSON[i].state != 'private') {
+				if (dataJSON[i].name === nickname)
+					console.log(`  ${dataJSON[i].name} > `, `${dataJSON[i].message}`);
+				else
+					console.log('\x1b[33m%s\x1b[0m', `  ${dataJSON[i].name} > `, `${dataJSON[i].message}`);
+			} 
+			else if ((dataJSON[i].reciever === nickname) || (dataJSON[i].name === nickname)) {
+				console.log('\x1b[36m%s\x1b[0m', `  ${dataJSON[i].name} > `, `${dataJSON[i].message}`);
+			}
+		}
+		
+		updateData = true;
+
+		if (updateData === true)
+			readAndSend();
+	})
+
+
+
+
+
+	function readAndSend () {
+		updateData = false;
+
+		rl.question(chalk.red(`  ${nickname} > `), (answer) => {
+
+			if (answer === "$0") {
+				console.log(chalk.green("\т  Buy!..."))
+				client.end();
+				rl.close();
+				return
+			}
+
+
+			if (answer[0] == '#') {
+				answer = answer.split(' ');
+
+				var reciever = answer[0];
+					reciever = reciever.slice(1)
+
+				answer.splice(0, 1)
+					  .join(' ');
+
+				client.write(JSON.stringify({
+					'state': 'private',
+					'name': nickname,
+					'message': answer,
+					'reciever': reciever.toUpperCase()
+				}))	
+
+			} else {
+				client.write(JSON.stringify({
+					'state': 'broadcast',
+					'name': nickname,
+					'message': answer
+				}))	
+			}
+		});
+	}
+
+
+	client.on('end', function(data) {
+		console.log(chalk.green("  Disconnected from server..."))
+	})
 }
